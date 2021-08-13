dd128eb46189770d2d686a566a3c8b98f6b22a11
Asad <asadul201586@gmail.com>
2021-08-13 19:16:19 +0600

    history design complete

--- app/src/main/java/com/example/projectxbloodbank/view/fragment/HistoryFragment.java
+++ app/src/main/java/com/example/projectxbloodbank/view/fragment/HistoryFragment.java
@@ -10,6 +10,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
 import com.example.projectxbloodbank.R;
 import com.example.projectxbloodbank.databinding.FragmentHistoryBinding;
 import com.example.projectxbloodbank.others.GlobalValues;
@@ -30,5 +32,33 @@ public class HistoryFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         GlobalValues.currentFragment = "history";
+
+        binding.layoutDonations.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (binding.textDonations.getTag().equals("donations")) {
+                    binding.textDonations.setVisibility(View.GONE);
+                    binding.textDonationsZoom.setVisibility(View.VISIBLE);
+                    binding.textServiceTaken.setVisibility(View.VISIBLE);
+                    binding.textServiceTakenZoom.setVisibility(View.GONE);
+                    binding.textDonations.setTag("donations_zoom");
+                    binding.textServiceTaken.setTag("service");
+                }
+            }
+        });
+
+        binding.layoutServiceTaken.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (binding.textServiceTaken.getTag().equals("service")) {
+                    binding.textDonations.setVisibility(View.VISIBLE);
+                    binding.textDonationsZoom.setVisibility(View.GONE);
+                    binding.textServiceTaken.setVisibility(View.GONE);
+                    binding.textServiceTakenZoom.setVisibility(View.VISIBLE);
+                    binding.textDonations.setTag("donations");
+                    binding.textServiceTaken.setTag("service_zoom");
+                }
+            }
+        });
     }
 }
